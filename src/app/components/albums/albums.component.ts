8909c698c0ab3e82d40c9c5ca6cbab548110d19f
Anna <mrs.leonenko@gmail.com>
2020-03-27 12:57:43 -0400

    move data manipulation to service

--- src/app/components/albums/albums.component.ts
+++ src/app/components/albums/albums.component.ts
@@ -9,48 +9,23 @@ import {Album} from '../../album.model';
 })
 export class AlbumsComponent implements OnInit {
 
-  constructor(private albumService: AlbumService,
-              private cdRef: ChangeDetectorRef) { }
-  @Input() search: string;
+  constructor(private albumService: AlbumService) { }
+
   fromServer: Album[] = [];
   albums: Album[] = [];
   fetched = false;
   index: number;
 
-  click(){
-
+  click(event: any) {
+    event.stopPropagation();
   }
   ngOnInit(): void {
-    this.albumService.getAlbums()
+    this.albumService.getAll()
       .subscribe((data) => {
-        const {entry} = data.feed;
-        this.albums = this.parseForAlbums(entry);
+        this.albums = data;
         this.fromServer = [...this.albums];
         this.fetched = true;
       });
-  }
-
-  parseForAlbums(entry): Album[] {
-    console.log(entry);
-    return entry.map((album, key) => {
-      if (album) {
-        return {
-          key,
-          id: album.id.label,
-          name: album['im:name'].label,
-          artist: album['im:artist'].label,
-          image: album['im:image'] && album['im:image'][2] ? album['im:image'][2].label : '',
-          itemCount: +album['im:itemCount'].label,
-          price: +album['im:price'].attributes.amount,
-          currency: album['im:price'].attributes.currency,
-          rights: album.rights.label,
-          title: album.title.label,
-          link: album.link && album.link.attributes ? album.link.attributes.href : '',
-          category: album.category && album.category.attributes ? album.category.attributes.label : '',
-          releaseDate: album['im:releaseDate'].label,
-        };
-      }
-    });
   }
 
   showDetail(key) {
@@ -58,14 +33,10 @@ export class AlbumsComponent implements OnInit {
     this.index = key;
   }
 
-  setSearch(event: any) {
-    this.search = event.target.value;
-  }
-
-  filter() {
-    if (!this.search || this.search === '') { this.albums = this.fromServer; } else {
+  filter(search) {
+    if (!search || search === '') { this.albums = this.fromServer; } else {
       this.albums = this.fromServer
-                        .filter(album => (album.name.toLowerCase().includes(this.search) || album.artist.includes(this.search)));
+                        .filter(album => (album.name.toLowerCase().includes(search) || album.artist.includes(search)));
     }
   }
 }
--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Album} from '../album.model';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,35 @@ export class AlbumService {
 
   getAlbums(): Observable<any> {
     return this.http.get<any>(this.url);
+  }
+
+  getAll(): Observable<Album[]>  {
+    return this.getAlbums()
+      .pipe(map ((data) => {
+        const {entry} = data.feed;
+        return this.parseForAlbums(entry);
+      }));
+  }
+
+  parseForAlbums(entry): Album[] {
+    return entry.map((album, key) => {
+      if (album) {
+        return {
+          key,
+          id: album.id.label,
+          name: album['im:name'].label,
+          artist: album['im:artist'].label,
+          image: album['im:image'] && album['im:image'][2] ? album['im:image'][2].label : '',
+          itemCount: +album['im:itemCount'].label,
+          price: +album['im:price'].attributes.amount,
+          currency: album['im:price'].attributes.currency,
+          rights: album.rights.label,
+          title: album.title.label,
+          link: album.link && album.link.attributes ? album.link.attributes.href : '',
+          category: album.category && album.category.attributes ? album.category.attributes.label : '',
+          releaseDate: album['im:releaseDate'].label,
+        };
+      }
+    });
   }
 }
